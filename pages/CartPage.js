47c222d43def3d2fe77281f71d1477df946f01be
Владимир Тимофеев <basch19900@gmail.com>
2026-08-12 03:41:18 +0300

    added cartpage

---
+++ pages/CartPage.js
@@ -0,0 +1,13 @@
+const {expect} = require('@playwright/test');
+export class CartPage {
+    constructor(page) {
+        this.cartList = page.locator('[data-test="cart-list"]');
+        this.checkOutButton = page.locator('[data-test="checkout"]')
+        this.continueShoppingButton = page.locator('[data-test="continue-shopping"]')
+        
+    }
+    async goToCheckout() {
+        await this.checkOutButton.click();
+    }
+}
+
