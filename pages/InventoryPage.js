0a494943b88540c7a8d755aa0f30e8c91cbfa2c1
Владимир Тимофеев <basch19900@gmail.com>
2026-08-12 05:47:48 +0300

    added tests for login page, some edits of code

--- pages/InventoryPage.js
+++ pages/InventoryPage.js
@@ -2,11 +2,9 @@ const {expect} = require('@playwright/test');
 export class InventoryPage {
     constructor(page) {
         this.page = page;
-        this.pageTitle = page.getByText('Swag Labs');
+        this.pageTitle = page.locator('[data-test="title"]');
         this.shoppingCart = page.locator('[data-test="shopping-cart-link"]')
         this.inventoryList = page.locator('[data-test="inventory-container"]')
-        //this.addToCartButton = page.locator(getByText('Add to cart'))
-        //делайем динамическую кнопку (будет работать для любого товара)
         this.addToCartButton = (itemName) => {
             //у всех локаторов похожий паттерн add-to-cart-sauce-labs-bike-light
             const anyName = itemName.toLowerCase().replace(/\s/g, '-');
--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -1,13 +1,21 @@
 import {test , expect} from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { InventoryPage } from '../pages/InventoryPage';
 
-test ('Login' , async ({page}) => {
+test ('Full purchase flow' , async ({page}) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-    //await loginPage.login('standard_user', 'secret_sauce');
-    //err checkout
     await loginPage.login('standard_user', 'secret_sauce');
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    const inventoryPage = new InventoryPage(page);
+    const title = await inventoryPage.getPageTitle();
+    expect(title).toBe('Products');
+    //hight low , vice versa
+    await page.locator('[data-test="product-sort-container"]').selectOption('hilo');
+    const mostExpensiveItem = await page.locator('.inventory_item_name').first().textContent();
+    await inventoryPage.addItemToCart(mostExpensiveItem);
+    await inventoryPage.openCart();
+
     
 });
 
