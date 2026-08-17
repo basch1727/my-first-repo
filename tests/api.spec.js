2e5c0953df69a5f12712e6216d01fc7664c0bd0f
Владимир Тимофеев <basch19900@gmail.com>
2026-08-17 17:00:03 +0300

    feat: completed CRUD tests for restful-booker API

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,11 +1,11 @@
 import {test, expect} from '@playwright/test';
-import { request } from 'node:http';
-
+// используем .serial чтобы тесты шли друг за другом, иначем будут проблемы с ID 
+// так как по дефолту они выполняются паралельно и переменная не успевает переопределиться
 test.describe.serial('API - tests', () => {
     const baseUrl = 'https://restful-booker.herokuapp.com';
     const authUrl = 'https://restful-booker.herokuapp.com/auth';
     let bookingId;
-
+    //Создание бронирования
     test ('Creating new booking' , async ({ request }) => {
         
         const bookingData = {
@@ -19,52 +19,56 @@ test.describe.serial('API - tests', () => {
             },
             "additionalneeds" : "Breakfast"
         };
+        //отправляем post запрос
         const bookPost = await request.post(`${baseUrl}/booking` , {
             data:bookingData
         });
-
-        console.log(`Статус-код: ${bookPost.status()}`);
+        //проверка статуса запроса
+        console.log(`status-code: ${bookPost.status()}`);
         expect(bookPost.status()).toBe(200);
-
+        //конвертируем в обьект
         const responseBody = await bookPost.json();
-        console.log('Тело ответа:', responseBody);
-        
+        console.log('response body:', responseBody);
+        //проверка есть ли ID и совпадение
         expect(responseBody).toHaveProperty('bookingid');
         expect(responseBody.booking).toMatchObject(bookingData)
-
+        //сохраняем переменную с ID
         bookingId = responseBody.bookingid;
         console.log(`current id : ${bookingId}`)
     
     });
-    
+    //Получение информации о бронировании
     test('Get usage', async ({ request }) => {
         console.log(`виден ли : ${bookingId}`);
-
+        //отправляем get запрос
         const response = await request.get(`${baseUrl}/booking/${bookingId}`);
-
-        console.log(`Статус-код: ${response.status()}`);
+        //проверка статуса запроса
+        console.log(`status-code: ${response.status()}`);
         expect(response.status()).toBe(200);
-
+        //придаём пасте смерти - человеческий вид
         const responseBody = await response.json();
-        console.log('Тело ответа:', responseBody);
-
+        console.log('response body:', responseBody);
+        //проверяем на соответсвие
         expect(responseBody).toHaveProperty('firstname');
         expect(responseBody).toHaveProperty('lastname');
         expect(responseBody).toHaveProperty('totalprice');
 
     });
 
+    // Обновление бронирования
     test ('put usage' , async ({ request }) => {
         const authData = {
             "username" : "admin",
             "password" : "password123"
         }
+        // создаем post запрос
         const authPost = await request.post(`${authUrl}` , {
             data: authData
         });
-        
+        // приобразовываем
         const authBody = await authPost.json();
         console.log('auth:', authBody);
+        //сохраняем токен в переменную
         const token = authBody.token;
         console.log(`Token received: ${token}`);
 
@@ -79,23 +83,47 @@ test.describe.serial('API - tests', () => {
             },
             additionalneeds: "Breakfast"
         };
+        //создаем put запрос
         const putRequest = await request.put(`${baseUrl}/booking/${bookingId}`, {
             headers: {
                 Cookie: `token=${token}`
             },
             data: updatedData   
         });
-
+        //преобразовываем
         const putReqRes = await putRequest.json();
-        console.log('Тело ответа:', putReqRes);
-        console.log(`Статус-код PUT: ${putRequest.status()}`);
+        console.log('response body:', putReqRes);
+        // проверяем статус и успех
+        console.log(`put status-code: ${putRequest.status()}`);
         expect(putRequest.status()).toBe(200);
-
+        // проверяем соответствие 
         expect(putReqRes).toMatchObject(updatedData)
     
-    })
-    
+    });
+    // Удаление бронирования
+    test('delete usage', async ({ request }) => {
+        const authData = {
+            "username" : "admin",
+            "password" : "password123"
+        }
+        // нам опять нужен токен
+        const authPost = await request.post(`${authUrl}` , {
+            data: authData
+        });
+        const authBody = await authPost.json();
+        const token = authBody.token;
+        console.log(`deletion token: ${token}`);
+        // создаем delete запрос
+        const deleteResponse = await request.delete(`${baseUrl}/booking/${bookingId}`, {
+            headers: {
+                'Cookie': `token=${token}`
+            }
+        });
+        // проверяем статус и код (по документации 201)
+        console.log (`deletion status: ${deleteResponse.status()}`);
+        expect(deleteResponse.status()).toBe(201);
 
+    }); 
 });
 
 
